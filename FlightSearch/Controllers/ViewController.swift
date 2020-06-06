24ed625dca3b9021dab9202485bbec9c277caffb
Eduardo <eduardojordan@hotmail.com>
2020-06-06 09:52:40 +0200

    Initial repo

---
+++ FlightSearch/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightSearch
+//
+//  Created by MAC on 06/06/2020.
+//  Copyright © 2020 EduardoJordan.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
